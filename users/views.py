efbd53ba428437038b3067fed79db4eb50765704
Michael Greene <gogreenenj@gmail.com>
2020-06-12 09:12:11 -0400

    Initial commit

---
+++ users/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
+
+# Original code block:
+#
+# def register(request):
+#    if request.method == 'POST':
+#        form = UserRegisterForm(request.POST)
+#        if form.is_valid():
+#            form.save()
+#            username = form.cleaned_data.get('username')
+#            messages.success(request, f'Your account has been created, thank you! Please log in.')
+#            return redirect('login')
+#    else:
+#        form = UserRegisterForm()
+#    return render(request, 'users/register.html', {'form': form})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate with provided form fields
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'])
+            # Login with authentication
+            login(request, new_user)
+            # User feedback flash message
+            messages.success(request, f'You are logged in.')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST,
+            request.FILES,
+            instance=request.user.profile,
+        )
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request, f'Your account has been updated.')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+
+    return render(request, 'users/profile.html', context)
+
+
+
+# Messages.success
+# Messages.info
+# Messages.warning
+# Messages.error
+# Messages.debug
