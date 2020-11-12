cd3ed54a19f45f3bb935a2b240a845abdad29bde
Michael <GoGreeneNJ@gmail.com>
2020-11-11 21:13:41 -0500

    env adjustments

--- django_launch/settings.py
+++ django_launch/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'sx@fuocj%x1agly(-u-*3h3@3$mrx_ssnez$x9xcud0z+h!*rt'
+SECRET_KEY = os.getenv('DL_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,8 +136,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-print(EMAIL_HOST_USER)
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_HOST_USER = os.getenv('DL_EMAIL')
+EMAIL_HOST_PASSWORD = os.getenv('DL_PASS')
 
 
